85f692aab02cc669331c5e6f53102f44c433bc54
claus <claus@JC>
2024-02-19 17:16:21 -0300

    salvar

---
+++ src/main/java/com/jc/Ecommerce/entity/OrderItem.java
@@ -0,0 +1,66 @@
+package com.jc.Ecommerce.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem {
+	@EmbeddedId
+	private OrderItemPK id = new OrderItemPK();
+	private Integer quantity;
+	private Double price;
+//	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+//	private Payment payment;
+	
+	public OrderItem() {
+	}
+
+	public OrderItem(Order order, Product product, Integer quantity, Double price) {
+		id.setOrder(order);
+		id.setProduct(product);
+		this.quantity = quantity;
+		this.price = price;
+		}
+
+	public Order getOrder() {
+		return id.getOrder();
+	}
+
+	public void setOrder(Order order) {
+		id.setOrder(order);
+	}
+
+	public OrderItemPK getId() {
+		return id;
+	}
+
+	public void setId(OrderItemPK id) {
+		this.id = id;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+
+	
+	
+	
+	
+}
